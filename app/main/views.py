41c6a6e01a59401949f305640e827fe596fd1dfa
educationPythDjango <educationPyth@gmail.com>
2024-08-10 14:44:59 +0800

    begin

---
+++ app/main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'главная страница магазина',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'is_authenticated': True,
+
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('about page')
