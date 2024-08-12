1002aeffe54516233b2e4388fb7824cc8b9ad810
pythonDjango <educationPyth@gmail.com>
2024-08-12 14:30:45 +0800

    debug toolbar

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from main import views
+from debug_toolbar.toolbar import debug_toolbar_urls
+from app.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',  include('main.urls', namespace='main')),
     path('catalog/',  include('goods.urls', namespace='catalog'))
 ]
+
+if DEBUG:
+    urlpatterns += debug_toolbar_urls()
